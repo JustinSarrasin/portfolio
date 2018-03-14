44057ac649a7cd41f47a9c25521d9bd51436d9f6
Justin Sarrasin <sarrasin.justin@gmail.com>
2018-03-14 00:56:18 -0400

    footer fixed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,31 +1,7 @@
 'use strict';
 
-// const portfolio = {};
-
-// portfolio.smoothScroll = function() {
-//    $(function () {
-//       $('a').smoothScroll({
-//          offset: 100
-//       });
-//    });
-// }
-
-// portfolio.init = function () {
-//    portfolio.smoothScroll();
-// }
 $(function () {
    $('a').smoothScroll({
       speed: 1500
    });
 });
-
-// $(function () {
-//    $('a').smoothScroll({
-//       speed: 5000,
-//    });
-// });
-
-// $(function () {
-//    portfolio.init();
-// });    
-// console.log();
